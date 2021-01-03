b42739a3485ca103d8a2e6edf9aae1a2db13b204
Tanmay Vijayvargiya <tanmayvijayvargiya@pop-os.localdomain>
2021-01-03 23:08:32 +0530

    replace wget with curl

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ~/.tanmay/lily58
-wget https://raw.githubusercontent.com/tanmayv/lily58-host/main/index.js -P ~/.tanmay/lily58
+curl -o ~/.tanmay/lily58/index.js https://raw.githubusercontent.com/tanmayv/lily58-host/main/index.js
 pushd ~/.tanmay/lily58
 npm i -g pm2
 pm2 start index.js
